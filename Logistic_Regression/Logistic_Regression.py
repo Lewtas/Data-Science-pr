da247613c1f83d95f21c8c6f558090ee12529850
Lewtas <vlad.koza.2002@gmail.com>
2022-01-14 15:01:05 +0200

    pep8 logistic reg

--- Logistic_Regression/Logistic_Regression.py
+++ Logistic_Regression/Logistic_Regression.py
@@ -2,16 +2,17 @@ import numpy as np
 import h5py
 import matplotlib.pyplot as plt
 
+
 def load_data():
     train_dataset = h5py.File("train_cats.h5", "r")
-    train_set_x = np.array(train_dataset["train_set_x"][:]) # your train set features
-    train_set_y = np.array(train_dataset["train_set_y"][:]) # your train set labels
+    train_set_x = np.array(train_dataset["train_set_x"][:])  # your train set features
+    train_set_y = np.array(train_dataset["train_set_y"][:])  # your train set labels
 
     test_dataset = h5py.File("test_cats.h5", "r")
-    test_set_x = np.array(test_dataset["test_set_x"][:]) # your test set features
-    test_set_y = np.array(test_dataset["test_set_y"][:]) # your test set labels
+    test_set_x = np.array(test_dataset["test_set_x"][:])  # your test set features
+    test_set_y = np.array(test_dataset["test_set_y"][:])  # your test set labels
 
-    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    classes = np.array(test_dataset["list_classes"][:])  # the list of classes
 
     test_set_x = test_set_x.reshape(test_set_x.shape[0], -1).T
     train_set_x = train_set_x.reshape(train_set_x.shape[0], -1).T
@@ -19,6 +20,7 @@ def load_data():
     test_set_y = test_set_y.reshape((1, test_set_y.shape[0]))
 
     return train_set_x, train_set_y, test_set_x, test_set_y, classes
+
 
 def sigmoid(z):
     """
@@ -35,13 +37,14 @@ def initialize_with_zeros(dim):
     This function creates a vector of zeros of shape (dim, 1) for w and initializes b to 0.
     """
 
-    w = np.zeros((dim,1))
+    w = np.zeros((dim, 1))
     b = 0
 
     assert(w.shape == (dim, 1))
     assert(isinstance(b, float) or isinstance(b, int))
 
     return w, b
+
 
 def propagate(w, b, X, Y):
     """
@@ -51,11 +54,11 @@ def propagate(w, b, X, Y):
     m = X.shape[1]
 
     # FORWARD PROPAGATION (FROM X TO COST)
-    A =  sigmoid(np.dot(w.T,X)+b)    # compute activation
+    A = sigmoid(np.dot(w.T, X)+b)    # compute activation
     cost = np.sum((Y*np.log((A)) - (Y-1)*np.log((-(A-1)))))/(-m)  # compute cost
 
     # BACKWARD PROPAGATION (TO FIND GRAD)
-    dw = np.dot(X,(A-Y).T)/Y.size
+    dw = np.dot(X, (A-Y).T)/Y.size
     db = np.sum(A-Y)/Y.size
 
     assert(dw.shape == w.shape)
@@ -78,9 +81,8 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost=False):
 
     for i in range(num_iterations):
 
-
         # Cost and gradient calculation
-        grads, cost = propagate(w,b,X,Y)
+        grads, cost = propagate(w, b, X, Y)
 
         # Retrieve derivatives from grads
         dw = grads["dw"]
@@ -96,7 +98,7 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost=False):
 
         # Print the cost every 100 training iterations
         if print_cost and i % 100 == 0:
-            print ("Cost after iteration %i: %f" %(i, cost))
+            print("Cost after iteration %i: %f" % (i, cost))
 
     params = {"w": w,
               "b": b}
@@ -106,62 +108,61 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost=False):
 
     return params, grads, costs
 
+
 def predict(w, b, X):
     '''
     Predict whether the label is 0 or 1 using learned logistic regression parameters (w, b)
     '''
 
     m = X.shape[1]
-    Y_prediction = np.zeros((1,m))
+    Y_prediction = np.zeros((1, m))
 
     # Compute vector "A" predicting the probabilities of a cat being present in the picture
-    A = sigmoid(np.dot(w.T,X)+b)
+    A = sigmoid(np.dot(w.T, X)+b)
 
     # Convert probabilities A[0,i] to actual predictions p[0,i]
-    Y_prediction= A>0.5
-    Y_prediction.dtype='int8'
+    Y_prediction = A > 0.5
+    Y_prediction.dtype = 'int8'
     ### Variable with probabilities called Y_prediction
-
 
     assert(Y_prediction.shape == (1, m))
 
     return Y_prediction
+
 
 def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
     """
     Builds the logistic regression model by calling the function you've implemented previously
     """
 
-
     # initialize parameters with zeros
     w, b = initialize_with_zeros(X_train.shape[0])
 
     # Gradient descent
-    parameters, grads, costs = optimize(w,b,X_train,Y_train,num_iterations,learning_rate,print_cost)
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
 
     # Retrieve parameters w and b from dictionary "parameters"
     w = parameters["w"]
     b = parameters["b"]
 
     # Predict test/train set examples
-    Y_prediction_test = predict(w,b,X_test)
-    Y_prediction_train = predict(w,b,X_train)
-
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
 
     # Print train/test Errors
     print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
     print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
 
-
     d = {"costs": costs,
          "Y_prediction_test": Y_prediction_test,
-         "Y_prediction_train" : Y_prediction_train,
-         "w" : w,
-         "b" : b,
-         "learning_rate" : learning_rate,
+         "Y_prediction_train": Y_prediction_train,
+         "w": w,
+         "b": b,
+         "learning_rate": learning_rate,
          "num_iterations": num_iterations}
 
     return d
+
 
 train_set_x, train_set_y, test_set_x, test_set_y, classes = load_data()
 train_set_x = train_set_x / 255.
@@ -170,8 +171,9 @@ test_set_x = test_set_x / 255.
 d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=2000, learning_rate=0.005, print_cost=True)
 
 index = 7
-plt.imshow(test_set_x[:,index].reshape((64, 64, 3)))
-print ("y = " + str(test_set_y[0,index]) + ", you predicted that it is a \"" + classes[int(d["Y_prediction_test"][0,index])].decode("utf-8") +  "\" picture.")
+plt.imshow(test_set_x[:, index].reshape((64, 64, 3)))
+print("y = " + str(test_set_y[0, index]) + ", you predicted that it is a \""
+      + classes[int(d["Y_prediction_test"][0, index])].decode("utf-8") + "\" picture.")
 
 costs = np.squeeze(d["costs"])
 plt.plot(costs)
@@ -183,9 +185,9 @@ plt.show()
 learning_rates = [0.01, 0.001, 0.0001]
 models = {}
 for i in learning_rates:
-    print ("learning rate is: " + str(i))
+    print("learning rate is: " + str(i))
     models[str(i)] = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=1500, learning_rate=i, print_cost=False)
-    print ('\n' + "-------------------------------------------------------" + '\n')
+    print('\n' + "-------------------------------------------------------" + '\n')
 
 for i in learning_rates:
     plt.plot(np.squeeze(models[str(i)]["costs"]), label=str(models[str(i)]["learning_rate"]))
